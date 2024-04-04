57cf810d7d74fdb2335b9ba914beece140c37e01
Dmitry Lebedev <dl@adios.ru>
2024-04-04 17:09:52 +0500

    Headers added.

--- src/MySQL/Config.php
+++ src/MySQL/Config.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1);
-
+/**
+ * (c) 2005-2024 Dmitry Lebedev <dl@adios.ru>
+ * This source code is part of the Ultra data package.
+ * Please see the LICENSE file for copyright and licensing information.
+ */
 namespace Ultra\Data\MySQL;
 
 use Ultra\Data\Config as DataConfig;
